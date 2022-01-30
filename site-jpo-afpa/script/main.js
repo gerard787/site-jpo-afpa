7a30ed53635a551e6b2817a9a3e5b692505615ec
thief38 <98711113+thief38@users.noreply.github.com>
2022-01-30 20:58:04 +0100

    Add files via upload

---
+++ site-jpo-afpa/script/main.js
@@ -0,0 +1,29 @@
+let jeunes = document.getElementById("jeunes");
+let profil_jeunes = document.getElementById("profil_jeunes");
+
+profil_jeunes.addEventListener("mouseover", () => {jeunes.style.display = "block";});
+profil_jeunes.addEventListener("mouseout", () => {jeunes.style.display = "none";});
+
+let salarie = document.getElementById("salarie");
+let profil_salarie = document.getElementById("profil_salarie");
+
+profil_salarie.addEventListener("mouseover", () => {salarie.style.display = "block";});
+profil_salarie.addEventListener("mouseout", () => {salarie.style.display = "none";});
+
+let reconversion = document.getElementById("reconversion");
+let profil_reconversion = document.getElementById("profil_reconversion");
+
+profil_reconversion.addEventListener("mouseover", () => {reconversion.style.display = "block";});
+profil_reconversion.addEventListener("mouseout", () => {reconversion.style.display = "none";});
+
+let migrants = document.getElementById("migrants");
+let profil_migrants = document.getElementById("profil_migrants");
+
+profil_migrants.addEventListener("mouseover", () => {migrants.style.display = "block";});
+profil_migrants.addEventListener("mouseout", () => {migrants.style.display = "none";});
+
+let handicap = document.getElementById("handicap");
+let profil_handicap = document.getElementById("profil_handicap");
+
+profil_handicap.addEventListener("mouseover", () => {handicap.style.display = "block";});
+profil_handicap.addEventListener("mouseout", () => {handicap.style.display = "none";});
